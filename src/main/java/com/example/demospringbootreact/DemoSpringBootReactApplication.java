017d6c547574868e4138821e23a9766fe3e50150
sambujanni <siva.janni11@gmail.com>
2020-09-12 19:48:56 +0530

    initial check in

---
+++ src/main/java/com/example/demospringbootreact/DemoSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootReactApplication.class, args);
+	}
+
+}
